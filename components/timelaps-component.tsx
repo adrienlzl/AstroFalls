278d2bfda33e556c86ea10e04a4dc9e5ec074962
adrienlzl <lazailleadrien@gmail.com>
2025-02-10 10:58:40 +0100

    feature(app) add new chart

--- components/timelaps-component.tsx
+++ components/timelaps-component.tsx
@@ -2,6 +2,7 @@ import {Meteorite} from "@/lib/interfaces/meteorite-interface";
 import GraphYearByLength from "@/components/all-chart/chart-year-lenght";
 import GraphYearByMass from "@/components/all-chart/chart-year-mass-component";
 import ChartYearFallNotFall from "@/components/all-chart/chart-year-fall-not-fall";
+import ChartCountryByNumber from "@/components/all-chart/chart-country-counter.component";
 
 
 export default function TimelapsComponent({meteorites}: { meteorites: Meteorite[] }) {
@@ -14,6 +15,9 @@ export default function TimelapsComponent({meteorites}: { meteorites: Meteorite[
             </div>
             <div className="mb-12">
                 <ChartYearFallNotFall meteorites={meteorites}/>
+            </div>
+            <div className={"mb-12"}>
+                <ChartCountryByNumber meteorites={meteorites}/>
             </div>
 
         </div>

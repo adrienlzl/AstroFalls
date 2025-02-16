12702c37129ff1752c174fc430b6de5ad1fdfaf0
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-16 12:54:22 +0100

    add some meta in <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,10 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Astro Falls",
+  authors: [{ name: "Adrien Lazaille, Camille Hoareau, Corentin Beyries et Emmanuel Lefevre" }],
+  keywords: ["NextJS", " Tailwind", " MongoDB"],
   description: "Explorez les chutes de météorites à travers le monde grâce à une visualisation interactive. Analysez les données géographiques, temporelles et caractéristiques des météorites avec des graphiques, tableau et des données clés",
+  themeColor: "#95cb11"
 };
 
 

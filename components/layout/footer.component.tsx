30006ac6f126cf6622c4f81c39ba9894e2b68980
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-16 13:27:20 +0100

    semantic

--- components/layout/footer.component.tsx
+++ components/layout/footer.component.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function FooterComponent() {
 	return (
-		<div className="bg-dark w-full px-4 flex justify-center py-6">
+		<footer className="bg-dark w-full px-4 flex justify-center py-6">
 			<div className="text-sm font-semibold py-1 flex flex-wrap items-center text-gray-400">
 				<span className="text-black mr-1">Copyright © 2025</span>
 				<span className="text-black mr-1">Made with</span>
@@ -23,6 +23,6 @@ export default function FooterComponent() {
 					className="text-gray-600 hover:text-black ml-1">Adrien Lazaille
 				</Link>
 			</div>
-		</div>
+		</footer>
 	)
 }

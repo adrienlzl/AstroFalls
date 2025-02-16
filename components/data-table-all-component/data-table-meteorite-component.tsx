265dbd700919a54dab603a45759c1280b154a431
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-16 13:39:11 +0100

    unused div + id on table

--- components/data-table-all-component/data-table-meteorite-component.tsx
+++ components/data-table-all-component/data-table-meteorite-component.tsx
@@ -62,48 +62,46 @@ export function DataTable<TData extends RowData, TValue>({
 
 	return (
 		<div className="mt-2">
-			<div className="rounded-md border">
-				<Table>
-					<TableHeader>
-						{typedTable.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => (
-									<TableHead key={header.id}>
-										{header.isPlaceholder
-											? null
-											: flexRender(
-												header.column.columnDef.header,
-												header.getContext()
-											)}
-									</TableHead>
+			<Table id="table">
+				<TableHeader>
+					{typedTable.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<TableHead key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+								</TableHead>
+							))}
+						</TableRow>))}
+				</TableHeader>
+				<TableBody>
+					{typedTable.getRowModel().rows?.length ? (
+						typedTable.getRowModel().rows.map((row) => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && "selected"}>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(
+											cell.column.columnDef.cell,
+											cell.getContext()
+										)}
+									</TableCell>
 								))}
-							</TableRow>))}
-					</TableHeader>
-					<TableBody>
-						{typedTable.getRowModel().rows?.length ? (
-							typedTable.getRowModel().rows.map((row) => (
-								<TableRow
-									key={row.id}
-									data-state={row.getIsSelected() && "selected"}>
-									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext()
-											)}
-										</TableCell>
-									))}
-								</TableRow>))
-						) : (
-							<TableRow>
-								<TableCell colSpan={columns.length} className="h-24 text-center">
-									No results.
-								</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</div>
+							</TableRow>))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 			<div className="mt-5 flex justify-end">
 				<DataTablePagination<TData> table={typedTable} />
 			</div>

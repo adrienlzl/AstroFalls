c0593eea673474d09c6b3667b120462da03739d6
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-16 14:28:35 +0100

    logo rounded

--- components/home.component.tsx
+++ components/home.component.tsx
@@ -4,10 +4,11 @@ import Image from "next/image";
 
 export default function HomeComponent() {
 	return (
-		<div className={"flex justify-center items-center"}>
+		<div className="flex justify-center items-center">
 			<Image
 				src="/images/astroFallLogo.webp"
 				alt="AstroFalls Logo"
+				className="w-150 h-150 rounded-full object-cover"
 				width={500}
 				height={300}
 				priority />

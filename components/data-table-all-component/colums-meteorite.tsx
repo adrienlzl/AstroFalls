aeb2fa3420aec0b5b120bf783bf8c4821c41ae5d
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-19 18:02:57 +0100

    pipe on fall/find

--- components/data-table-all-component/colums-meteorite.tsx
+++ components/data-table-all-component/colums-meteorite.tsx
@@ -27,6 +27,15 @@ export const columns: ColumnDef<Meteorite>[] = [
 	{
 		accessorKey: "ff",
 		header: "Découverte",
+		cell: ({ row }) => {
+			const value = row.getValue("ff") as string | undefined;
+			if (!value) return "NC";
+
+			const lowerCaseValue = value.toLowerCase();
+			if (lowerCaseValue === "find") return "Oui";
+			if (lowerCaseValue === "fall") return "Non";
+			return "NC";
+		}
 	},
 	{
 		accessorKey: "Class",

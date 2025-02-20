a742712f50d3d780c998973c5136e5ca84e6c607
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-20 14:43:21 +0100

    maj

--- lib/action/meteorite.action.tsx
+++ lib/action/meteorite.action.tsx
@@ -1,21 +1,29 @@
 'use server'
 
-import {CollectionName} from '@/lib/collection.enum';
-
-import {connectToDatabase} from '@/lib/mongodb';
-import {Meteorite} from "@/lib/interfaces/meteorite-interface";
+import { CollectionName } from '@/lib/collection.enum';
+import { connectToDatabase } from '@/lib/mongodb';
+import { Meteorite } from "@/lib/interfaces/meteorite-interface";
 
 
 export async function getAllMeteorite(): Promise<Meteorite[]> {
 	try {
+		console.time("⏳ Récupération des météorites");
+
 		const db = await connectToDatabase();
 
-		const MeteoriteDatas: Meteorite[] = await db.collection<Meteorite>(CollectionName.METEORITE_FRONT).find({}).toArray();
+		console.time("⏳ Exécution de la requête MongoDB");
+		const MeteoriteDatas: Meteorite[] = await db.collection<Meteorite>(CollectionName.METEORITE_FRONT)
+			.find({})
+			.toArray();
+		console.timeEnd("⏳ Exécution de la requête MongoDB");
 
-		return MeteoriteDatas.map((MeteoriteData) => ({
+		const result =  MeteoriteDatas.map((MeteoriteData) => ({
 			...MeteoriteData,
-			_id: MeteoriteData?._id?.toString(),
+			_id: MeteoriteData?._id?.toString()
 		}));
+
+		console.timeEnd("⏳ Récupération des météorites");
+		return result;
 	}
 
 	catch (error) {

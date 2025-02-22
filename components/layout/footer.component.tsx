271d0caae561aa3aa08d85f117cbd887147ae917
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-22 18:10:13 +0100

    maj style footer

--- components/layout/footer.component.tsx
+++ components/layout/footer.component.tsx
@@ -5,23 +5,23 @@ import Link from 'next/link'
 export default function FooterComponent() {
 	return (
 		<footer>
-			<div className="py-1">
+			<div id="footer-header">
 				<span id="copyright">Copyright © 2025</span>
-				<span className="mr-1 text-bold">Made with</span>
+				<span className="from">Made with</span>
 				<span id="heart">❤</span>
-				<span className="mr-1 text-bold">in Bordeaux</span>
+				<span className="from">in Bordeaux</span>
 			</div>
 			<div id="authors">
 				<Image
 					src="/images/linkedin.png"
-					alt="LinkedIn"
-					className="mr-2"
+					alt="Logo de LinkedIn"
 					width={22}
 					height={22}/>
 				<Link
 					href="https://www.linkedin.com/in/adrien-lazaille-11767266/"
 					target="_blank"
-					className="linkedin-link mr-1"
+					aria-label="Lien vers le profil Linkedin de Adrien Lazaille"
+					className="linkedin-links"
 					rel="noopener noreferrer">
 						Adrien Lazaille
 				</Link>
@@ -29,21 +29,24 @@ export default function FooterComponent() {
 				<Link
 					href="https://www.linkedin.com/in/emmanuel-l-06350b167/"
 					target="_blank"
-					className="linkedin-link mx-1"
+					aria-label="Lien vers le profil Linkedin de Emmanuel lefevre"
+					className="linkedin-links"
 					rel="noopener noreferrer">Emmanuel Lefevre
 				</Link>
 				<span>|</span>
 				<Link
 					href="https://www.linkedin.com/in/corentin-beyries/"
 					target="_blank"
-					className="linkedin-link mx-1"
+					aria-label="Lien vers le profil Linkedin de Corentin Beyries"
+					className="linkedin-links"
 					rel="noopener noreferrer">Corentin Beyries
 				</Link>
 				<span>|</span>
 				<Link
 					href="https://www.linkedin.com/in/camille-hoareau-487782253/"
 					target="_blank"
-					className="linkedin-link ml-1"
+					aria-label="Lien vers le profil Linkedin de Camille Hoareau"
+					className="linkedin-links"
 					rel="noopener noreferrer">Camille Hoareau
 				</Link>
 			</div>

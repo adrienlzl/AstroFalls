cbb57798161d64ac65cb42c88b2ea16420f49bda
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-22 12:41:00 +0100

    maj

--- lib/interfaces/meteorite-interface.tsx
+++ lib/interfaces/meteorite-interface.tsx
@@ -1,24 +1,24 @@
 export interface Meteorite {
 	_id?: string;
-	"(A)chondrite": string | null; // Achondrite ou Chondrite
-	id: number; // Identifiant unique
-	Class: string | null; // Classe de la météorite
-	Country: string; // Pays où elle a été trouvée
-	latitude: number; // Latitude de la localisation
-	longitude: number; // Longitude de la localisation
-	ff: "Find" | "Fall"; // Type (Find ou Fall)
-	Group: string | null; // Groupe de la météorite
-	Locality: string; // Localité
-	mck: string; // Code météorite
-	Month: string | null; // Mois de la découverte
-	Name: string; // Nom de la météorite
-	"Petrologic type": string | null; // Type pétrologique
-	"Recovered weight": string | null; // Poids récupéré avec unités (g, kg)
-	"Shock stage": string | null; // Stade de choc
-	Synonyms: string | null; // Synonymes ou autres noms
+	"(A)chondrite": string | null;     		// Achondrite ou Chondrite
+	id: number;
+	Class: string | null;
+	Country: string;
+	latitude: number;
+	longitude: number;
+	ff: "Find" | "Fall";
+	Group: string | null;
+	Locality: string;
+	mck: string; 													// Code météorite
+	Month: string | null;
+	Name: string;
+	"Petrologic type": string | null;
+	"Recovered weight": string | null;
+	"Shock stage": string | null;        	// Stade de choc
+	Synonyms: string | null;
 	Type: MeteoriteType | null;
-	wg: string | null; // Autres informations
-	Year: string; // Année de découverte
+	wg: string | null; 										// Autres informations
+	Year: string;
 }
 
 export type MeteoriteType = "Stone" | "Iron" | "Stony-Iron";

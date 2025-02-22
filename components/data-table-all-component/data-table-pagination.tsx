ebf70eae398ce38ddd62e61253a081b74f21f5de
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-22 22:30:32 +0100

    maj style table

--- components/data-table-all-component/data-table-pagination.tsx
+++ components/data-table-all-component/data-table-pagination.tsx
@@ -20,60 +20,58 @@ export function DataTablePagination<TData extends RowData>({
 	return (
 		<div id="pagination-container">
 			<div id="total-result">
-				{table.getPrePaginationRowModel().rows.length.toLocaleString("fr-FR")} résultats
+				{ table.getPrePaginationRowModel().rows.length.toLocaleString("fr-FR") } résultats
 			</div>
 			<div id="pagination">
 				<div id="rows">
 					<Select
-						value={`${table.getState().pagination.pageSize}`}
-						onValueChange={(value) => {
-							table.setPageSize(Number(value));
-						}}>
+						value={`${ table.getState().pagination.pageSize }`}
+						onValueChange={(value) => { table.setPageSize(Number(value)) }}>
 						<SelectTrigger className="h-8 w-[70px]">
-							<SelectValue placeholder={table.getState().pagination.pageSize} />
+							<SelectValue placeholder={ table.getState().pagination.pageSize } />
 						</SelectTrigger>
-						<SelectContent side="top">
+						<SelectContent id="number-page-dropdown" side="top">
 							<>
-							{[10, 20, 30, 40, 50].map((pageSize) => (
-								<SelectItem key={pageSize} value={`${pageSize}`}>
-									{pageSize}
-								</SelectItem>
-							))}
+								{[10, 20, 30, 40, 50].map((pageSize) => (
+									<SelectItem id="select-page-item" key={ pageSize } value={`${ pageSize }`}>
+										{ pageSize }
+									</SelectItem>
+								))}
 							</>
 						</SelectContent>
 					</Select>
 				</div>
-				<div className="flex w-[130px] items-center justify-center text-sm font-medium">
-					Page {table.getState().pagination.pageIndex + 1} of{" "} {table.getPageCount()}
+				<div id="page-count">
+					Page { table.getState().pagination.pageIndex + 1 } of{" "} { table.getPageCount() }
 				</div>
-				<div className="flex items-center space-x-2">
+				<div id="page" className="flex items-center space-x-2">
 					<Button
 						variant="outline"
 						className="previous hidden h-8 w-8 p-0 lg:flex"
-						onClick={() => table.setPageIndex(0)}
-						disabled={!table.getCanPreviousPage()}>
-						{"<<"}
+						onClick={ () => table.setPageIndex(0) }
+						disabled={ !table.getCanPreviousPage() }>
+						{ "<<" }
 					</Button>
 					<Button
 						variant="outline"
 						className="previous h-8 w-8 p-0"
-						onClick={() => table.previousPage()}
-						disabled={!table.getCanPreviousPage()}>
-						{"<"}
+						onClick={ () => table.previousPage() }
+						disabled={ !table.getCanPreviousPage() }>
+						{ "<" }
 					</Button>
 					<Button
 						variant="outline"
 						className="next h-8 w-8 p-0"
-						onClick={() => table.nextPage()}
-						disabled={!table.getCanNextPage()}>
-						{">"}
+						onClick={ () => table.nextPage() }
+						disabled={ !table.getCanNextPage() }>
+						{ ">" }
 					</Button>
 					<Button
 						variant="outline"
 						className="next hidden h-8 w-8 p-0 lg:flex"
-						onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-						disabled={!table.getCanNextPage()}>
-						{">>"}
+						onClick={ () => table.setPageIndex(table.getPageCount() - 1) }
+						disabled={ !table.getCanNextPage() }>
+						{ ">>" }
 					</Button>
 				</div>
 			</div>

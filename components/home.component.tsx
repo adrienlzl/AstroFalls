800850a3dbe143814c99d991329e329e96323ee8
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-25 08:37:25 +0100

    animation on logo

--- components/home.component.tsx
+++ components/home.component.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import styles from "@/app/animations.module.scss";
 
 
 export default function HomeComponent() {
@@ -9,6 +10,7 @@ export default function HomeComponent() {
 				id="logo"
 				src="/images/astroFallLogo.webp"
 				alt="AstroFalls Logo"
+				className={ styles.bounce }
 				width={500}
 				height={300}
 				priority />

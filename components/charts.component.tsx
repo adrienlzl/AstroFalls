b89d12572411fd1b02fcfb0dc009a8a8aba0eaa6
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-02-27 08:39:41 +0100

    renaming

--- components/charts.component.tsx
+++ components/charts.component.tsx
@@ -1,10 +1,10 @@
 import { Meteorite } from "@/lib/interfaces/meteorite-interface";
-import ChartFallsByCountry from "@/components/all-chart/chart-falls-by-country.component";
-import ChartTotalByType from "@/components/all-chart/chart-total-mass-by-type.component";
-import ChartTotalMassByType from "@/components/all-chart/chart-total-by-type.component";
-import ChartFallsByYear from "@/components/all-chart/chart-falls-by-year.component";
-import ChartFallsByYearAfter1974 from "@/components/all-chart/chart-falls-by-year-after-1974.component";
-import ChartTotalMassByYear from "@/components/all-chart/chart-total-mass-by-year.component";
+import ChartFallsByCountry from "@/components/all-charts/chart-falls-by-country.component";
+import ChartTotalByType from "@/components/all-charts/chart-total-mass-by-type.component";
+import ChartTotalMassByType from "@/components/all-charts/chart-total-by-type.component";
+import ChartFallsByYear from "@/components/all-charts/chart-falls-by-year.component";
+import ChartFallsByYearAfter1974 from "@/components/all-charts/chart-falls-by-year-after-1974.component";
+import ChartTotalMassByYear from "@/components/all-charts/chart-total-mass-by-year.component";
 
 
 export default function ChartsComponent({meteorites}: { meteorites: Meteorite[] }) {

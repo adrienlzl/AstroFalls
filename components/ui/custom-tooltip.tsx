c92b5cfaeb72dc446026b4a1eff9f1acaae7aa15
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-01 19:11:48 +0100

    custom tooltip

---
+++ components/ui/custom-tooltip.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useGenericColorsHook } from "@/lib/utils/use-generic-colors-hook";
+
+
+interface CustomTooltipProps {
+  active?: boolean;
+  payload?: { value: number }[];
+  label?: string;
+  cursorStyle?: React.CSSProperties;
+  labelText?: string;
+  formatter?: (value: number) => string;
+}
+
+export const CustomTooltip: React.FC<CustomTooltipProps> = ({
+  active,
+  payload,
+  label,
+  cursorStyle,
+  labelText = "Chutes : ",
+  formatter = (value) => value.toLocaleString()
+}) => {
+  const { primaryColor, secondaryColor, accentColor } = useGenericColorsHook();
+
+  if (active && payload && payload.length) {
+    return (
+      <div style={{ background: "white", padding: "8px", ...cursorStyle }}>
+        <p style={{ color: accentColor, fontWeight: "bold" }}>{ label }</p>
+        <p style={{ color: secondaryColor }}>{ labelText }
+          <span style={{ color: primaryColor }}>{ formatter(payload[0].value) }</span>
+        </p>
+      </div>
+    );
+  }
+  return null;
+};

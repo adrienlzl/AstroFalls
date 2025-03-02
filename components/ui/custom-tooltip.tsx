e42828271599a8e1716462d88cf0d5e198989a06
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-02 10:40:37 +0100

    maj custom tooltip

--- components/ui/custom-tooltip.tsx
+++ components/ui/custom-tooltip.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import { useGenericColorsHook } from "@/lib/utils/use-generic-colors-hook";
 
 
+type LabelColorType = "accent" | "country" | "type";
+
 interface CustomTooltipProps {
   active?: boolean;
-  payload?: { value: number }[];
+  payload?: { value: number | undefined }[];
   label?: string;
   cursorStyle?: React.CSSProperties;
   labelText?: string;
   formatter?: (value: number) => string;
+  labelColorType?: LabelColorType;
 }
+
 
 export const CustomTooltip: React.FC<CustomTooltipProps> = ({
   active,
@@ -17,16 +21,33 @@ export const CustomTooltip: React.FC<CustomTooltipProps> = ({
   label,
   cursorStyle,
   labelText = "Chutes : ",
-  formatter = (value) => value.toLocaleString()
+  formatter = (value) => value.toLocaleString(),
+  labelColorType = "accent"
 }) => {
   const { primaryColor, secondaryColor, accentColor } = useGenericColorsHook();
 
+  let labelColor: string;
+
+  switch (labelColorType) {
+    case "accent":
+      labelColor = accentColor;
+      break;
+    case "country":
+      labelColor = cursorStyle?.color || primaryColor;
+      break;
+    case "type":
+    default:
+      labelColor = primaryColor;
+      break;
+  }
+
   if (active && payload && payload.length) {
-    const formattedValue = `${formatter(payload[0].value)}`;
+    const value = payload[0].value;
+    const formattedValue = typeof value === "number" ? `${formatter(value)}` : "N/A";
 
     return (
       <div style={{ background: "white", padding: "8px", ...cursorStyle }}>
-        <p style={{ color: accentColor, fontWeight: "bold" }}>{ label }</p>
+        <p style={{ color: labelColor, fontWeight: "bold" }}>{ label }</p>
         <p style={{ color: secondaryColor }}>{ labelText }
           <span style={{ color: primaryColor }}>{ formattedValue }</span>
         </p>

47390bbf92b389362c23857e4caf2c0699c2efd3
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-04 09:36:55 +0100

    maj

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,13 @@ import Background from "@/components/ui/background.component";
 export const metadata: Metadata = {
   title: "Astro Fall",
   authors: [{ name: "Adrien Lazaille, Camille Hoareau, Corentin Beyries et Emmanuel Lefevre" }],
-  keywords: ["NextJS", " Tailwind", " MongoDB", "Scss", "TurboPack"],
-  description: "Explorez les chutes de météorites à travers le monde grâce à une visualisation interactive. Analysez les données géographiques, temporelles et caractéristiques des météorites avec des graphiques, tableaux et des données clés"
+  keywords: ["NextJS", "Tailwind", "MongoDB", "Scss", "TurboPack", "Recharts", "ShadUI", "Vercel"],
+  description: "Explorez les chutes de météorites à travers le monde grâce à une visualisation interactive. Analysez les données géographiques, temporelles et caractéristiques des météorites avec des graphiques, tableaux et des données clés."
 };
 
 export const viewport = {
   themeColor: "#02afac"
 };
-
 
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
 	return (
 		<>
-			<NavComponent data={meteoriteData}/>
+			<NavComponent data={ meteoriteData }/>
 			<FooterComponent />
 		</>
 	);

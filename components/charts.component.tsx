07e7486c53f174472f675d633af7be3ee57ff050
Emmanuel Lefevre <waiasiaa@protonmail.com>
2025-03-04 09:29:33 +0100

    maj défilement fluide des charts

--- components/charts.component.tsx
+++ components/charts.component.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useState, useCallback } from "react";
 import { Meteorite } from "@/lib/interfaces/meteorite-interface";
 import ChartFallsByCountry from "@/components/all-charts/chart-falls-by-country.component";
 import ChartFallsByHemisphere from "./all-charts/chart-falls-by-hemisphere.component";
@@ -9,19 +10,53 @@ import ChartTotalByType from "@/components/all-charts/chart-total-mass-by-type.c
 import ChartTotalMassByType from "@/components/all-charts/chart-total-by-type.component";
 import ChartTotalMassByYear from "@/components/all-charts/chart-total-mass-by-year.component";
 
+export default function ChartsComponent({ meteorites }: { meteorites: Meteorite[] }) {
+	const chartRefs = useRef<HTMLDivElement[]>([]);
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-export default function ChartsComponent({meteorites}: { meteorites: Meteorite[] }) {
+	const handleWheel = useCallback((event: WheelEvent) => {
+		const nextIndex = event.deltaY > 0 ? currentIndex + 1 : currentIndex - 1;
+
+		if (nextIndex >= 0 && nextIndex < chartRefs.current.length) {
+			requestAnimationFrame(() => {
+				setCurrentIndex(nextIndex);
+				chartRefs.current[nextIndex]?.scrollIntoView({
+					behavior: "smooth",
+					block: "center",
+				});
+			});
+		}
+	}, [currentIndex]);
+
+	useEffect(() => {
+		window.addEventListener("wheel", handleWheel, { passive: false });
+		return () => {
+			window.removeEventListener("wheel", handleWheel);
+		};
+	}, [handleWheel]);
+
 	return (
 		<div id="charts">
-			<ChartFallsByYear meteorites={ meteorites }/>
-			<ChartFallsByYearAfter1974 meteorites={ meteorites }/>
-			<ChartTotalMassByYear meteorites={ meteorites }/>
-			<ChartCountFindByMonth meteorites={ meteorites }/>
-			<ChartCountFallByMonth meteorites={ meteorites }/>
-			<ChartFallsByCountry meteorites={ meteorites }/>
-			<ChartTotalMassByType meteorites={ meteorites }/>
-			<ChartTotalByType meteorites={ meteorites }/>
-			<ChartFallsByHemisphere meteorites={ meteorites }/>
+			{[
+				ChartFallsByYear,
+				ChartFallsByYearAfter1974,
+				ChartTotalMassByYear,
+				ChartCountFindByMonth,
+				ChartCountFallByMonth,
+				ChartFallsByCountry,
+				ChartTotalMassByType,
+				ChartTotalByType,
+				ChartFallsByHemisphere,
+			].map((ChartComponent, index) => (
+				<div
+					key={ index }
+					ref={(el) => {
+						if (el) chartRefs.current[index] = el;
+					}}
+					className="chart-container" >
+					<ChartComponent meteorites={ meteorites } />
+				</div>
+			))}
 		</div>
-	)
+	);
 }
